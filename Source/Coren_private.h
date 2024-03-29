a89e6e7d67625b7de3fca92a02bd2e3536b6f3c9
Drmies <141207441+Hugopako@users.noreply.github.com>
2024-03-29 18:44:29 +0700

    Add files via upload

---
+++ Source/Coren_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef COREN_PRIVATE_H
+#define COREN_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"6.6.6.6"
+#define VER_MAJOR	6
+#define VER_MINOR	6
+#define VER_RELEASE	6
+#define VER_BUILD	6
+#define COMPANY_NAME	"Hugopako"
+#define FILE_VERSION	"6.6.6.6"
+#define FILE_DESCRIPTION	"Covid666  (NO SKID!)"
+#define INTERNAL_NAME	"Coren.exe"
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"Covid666.exe"
+#define PRODUCT_NAME	"inspiredby Hydrogen"
+#define PRODUCT_VERSION	"6.6.6.6"
+
+#endif /*COREN_PRIVATE_H*/
---
+++ Source/readme.txt
@@ -0,0 +1,12 @@
+()            ()
+() Coren ()
+()   .exe  ()
+()            ()
+                   
+Malware name: Coren
+Malware Type: Trojan
+Damage rate: Destructive
+Made in: C++
+Works Best in: Windows XP and Windows 7
+Created by: Hugopako
+Creation date: 29 March 2024
